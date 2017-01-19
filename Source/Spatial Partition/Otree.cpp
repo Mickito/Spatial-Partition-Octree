bbb0baf87f1cf97f244de37b1d1af85f2e4705c4
Mickito <100590093@student.swin.edu.au>
2017-01-19 20:35:36 +1100

    Prepared region to be subdivided

--- Source/Spatial Partition/Otree.cpp
+++ Source/Spatial Partition/Otree.cpp
@@ -40,4 +40,24 @@ void Otree::Tick(float DeltaTime)
 
 void Otree::BuildTree()
 {
+	if (Actors.Num() <= 0 || (dimensions.X <= 500 && dimensions.Y <= 500 && dimensions.Z <= 500))
+		return;
+
+	//Generates box dimensions
+	FVector Dimensions = Region.Max - Region.Min;
+	FVector center = Region.Min + (dimensions / 2.0);
+	
+	//Subdivide into 8 regions
+	subdivideBox.Insert(FBox(Region.Min, center), 0);
+	subdivideBox.Insert(FBox(center, Region.Max), 1);
+	subdivideBox.Insert(FBox(FVector(center.X, Region.Min.Y, center.Z), FVector(Region.Max.X, center.Y, Region.Max.Z)), 2);
+	subdivideBox.Insert(FBox(FVector(center.X, center.Y, Region.Min.Z), FVector(Region.Max.X, Region.Max.Y, center.Z)), 3);
+	subdivideBox.Insert(FBox(FVector(center.X, Region.Min.Y, Region.Min.Z), FVector(Region.Max.X, center.Y, center.Z)), 4);
+	subdivideBox.Insert(FBox(FVector(Region.Min.X, Region.Min.Y, center.Z), FVector(center.X, center.Y, Region.Max.Z)), 5);
+	subdivideBox.Insert(FBox(FVector(Region.Min.X, center.Y, Region.Min.Z), FVector(center.X, Region.Max.Y, center.Z)), 6);
+	subdivideBox.Insert(FBox(FVector(Region.Min.X, center.Y, center.Z), FVector(center.X, Region.Max.Y, Region.Max.Z)), 7);
+
+
+
+
 }

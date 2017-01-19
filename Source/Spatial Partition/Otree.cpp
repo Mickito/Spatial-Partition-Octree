9461f1adff57ecbce028225181c0386fa0833b25
Mickito <100590093@student.swin.edu.au>
2017-01-19 20:10:37 +1100

    Unreal Engine Boilerplate code added

---
+++ Source/Spatial Partition/Otree.cpp
@@ -0,0 +1,14 @@
+#include "Custom.h"
+#include "Otree.h"
+
+
+// Sets default values
+Otree::Otree()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+AActor* Otree::PreIntialize()
+{
+
+}
---
+++ Source/Spatial Partition/Otree.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Otree.generated.h"
+
+UCLASS()
+class CUSTOM_API AOtree : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	Otree();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+
+
+};
